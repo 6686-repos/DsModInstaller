e06f52356aa1c06a3b6452b7641586d869faeb85
alfredo6686 <107364321+alfredo6686@users.noreply.github.com>
2025-03-12 16:51:27 +0100

    Update runner.bat

--- runner.bat
+++ runner.bat
@@ -4,14 +4,19 @@ set "repo_path=%appdata%\dsmodinstaller\sheltupdate6686"
 
 :: Clone or pull repository
 if not exist "%repo_path%" (
+    echo Cloning repository...
     git clone https://github.com/6686-repos/sheltupdate6686 "%repo_path%"
 ) else (
+    echo Pulling latest changes...
     cd /d "%repo_path%"
     git pull
 )
 
-:: Run Node.js app
+:: Install dependencies and run Node.js app
 cd /d "%repo_path%"
+echo Installing dependencies...
+npm install
+echo Running Node.js application...
 node src/index.js
 
 endlocal
