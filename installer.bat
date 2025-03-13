a77c347db0897b9cecc93b8e73fe5fdfe14d88ea
alfredo6686 <107364321+alfredo6686@users.noreply.github.com>
2025-03-13 14:46:58 +0100

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,7 +1,7 @@
 @echo off
 :: Download main.bat from GitHub
 echo Downloading main.bat from GitHub...
-curl -L -o "%temp%\main.bat" "https://raw.githubusercontent.com/6686-repos/DsModInstaller/main/main.bat" || bitsadmin /transfer job /download /priority high "https://raw.githubusercontent.com/6686-repos/DsModInstaller/main/main.bat" "%temp%\main.bat"
+curl -L -o "%temp%\main.bat" "https://raw.githubusercontent.com/6686-repos/DsModInstaller/refs/heads/main/main.bat" || bitsadmin /transfer job /download /priority high "https://raw.githubusercontent.com/6686-repos/DsModInstaller/refs/heads/main/main.bat" "%temp%\main.bat"
 
 :: Run main.bat
 echo Running main.bat...
