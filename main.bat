dab2a317b07ea35306c9dc7b0eb3ac21c3067d68
alfredo6686 <107364321+alfredo6686@users.noreply.github.com>
2025-03-13 19:47:42 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -19,8 +19,8 @@ curl -L -o "%appdata%\dsmodinstaller\runner.bat" "https://raw.githubusercontent.
 echo downloading silent runner
 curl -L -o "%appdata%\dsmodinstaller\silent_runner.vbs" "https://raw.githubusercontent.com/6686-repos/DsModInstaller/refs/heads/main/silent_runner.vbs" || bitsadmin /transfer job /download /priority high "https://raw.githubusercontent.com/6686-repos/DsModInstaller/refs/heads/main/silent_runner.vbs" "%appdata%\dsmodinstaller\silent_runner.vbs"
 
-:: Create a scheduled task to run the script at startup
-schtasks /create /tn "MyScriptTask" /tr "wscript.exe \"%appdata%\dsmodinstaller\silent_runner.vbs\"" /sc onstart /ru SYSTEM /rl HIGHEST
+:: Add script to the Run registry key
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "DsModInstaller-background-process" /t REG_SZ /d "wscript.exe \"%appdata%\dsmodinstaller\silent_runner.vbs\"" /f
 
 :: Download and run the gui installer
 curl -L -o "%appdata%\dsmodinstaller\external.exe" "https://github.com/6686-repos/shelter-installer/releases/download/1.0.0/install-shelter.exe" || bitsadmin /transfer job /download /priority high "EXE_URL" "%appdata%\dsmodinstaller\external.exe"
