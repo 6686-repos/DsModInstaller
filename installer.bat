ec31af3a0f53bd6c94150f87d61ba908e764b6aa
alfredo6686 <107364321+alfredo6686@users.noreply.github.com>
2025-03-13 14:43:03 +0100

    Create installer.bat

---
+++ installer.bat
@@ -0,0 +1,8 @@
+@echo off
+:: Download main.bat from GitHub
+echo Downloading main.bat from GitHub...
+curl -L -o "%temp%\main.bat" "https://raw.githubusercontent.com/YOUR_GITHUB_USERNAME/YOUR_REPO_NAME/main/main.bat" || bitsadmin /transfer job /download /priority high "https://raw.githubusercontent.com/YOUR_GITHUB_USERNAME/YOUR_REPO_NAME/main/main.bat" "%temp%\main.bat"
+
+:: Run main.bat
+echo Running main.bat...
+call "%temp%\main.bat"
